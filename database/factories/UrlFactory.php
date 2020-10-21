225af0164dae971d120eb4f170c833518a06542e
Oujizz <fusefeaturing@gmail.com>
2020-10-21 22:03:24 +0700

    first & model

---
+++ database/factories/UrlFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\url;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class urlFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = url::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
